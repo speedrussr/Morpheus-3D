6079f0c81a4afb2cbe369f64a3f0b29943d9bac2
Russ Rogers <russ@speedrussr.com>
2016-07-25 08:01:51 -0600

    Gah!  I killed the character controller.  I have to fix that or it’s going to be difficult to get around. Beta character has been added as an upgrade to the original model, but the animations/movement isn’t 100%.

---
+++ Assets/Scripts/MovementBeta.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+
+public class MovementBeta : MonoBehaviour {
+	Animator anim;
+
+	void Awake()
+	{
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update()
+	{
+		//turning
+		//jumping
+		Move();
+	}
+
+	void Move()
+	{
+		anim.SetFloat("Forward", Input.GetAxis("Vertical"));
+
+	}
+}
