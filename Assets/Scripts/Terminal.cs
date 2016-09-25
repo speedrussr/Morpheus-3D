c7610a20a967a739ea66ca1b5f518351b117264b
Russ Rogers <russ@speedrussr.com>
2016-09-25 09:11:38 -0600

    Terminal Fix, minor changes - code update
    I lost multiple days worth of code due to a git commit (several
    actually) that caused me some consternation. Now I have to catch back
    up on those changes. This is a step in that direction. I’ll need to
    re-add the humanoid character, and re-layout the new terminal window.

--- Assets/Scripts/Terminal.cs
+++ Assets/Scripts/Terminal.cs
@@ -14,55 +14,69 @@ public class Terminal : MonoBehaviour {
 	//GameObject myPlayer;
 	// Variable to identify current Character Controller. 
 	public CharacterController cc;
-
-	void Awake() {
-		//cc.enabled = true;
-	}
+	public bool inTerminal = false;
 
 	// Use this for initialization
 	void Start () {
-
+		cc = GetComponent<CharacterController> ();
 		myCamera.enabled = false;
 		canvas.enabled = false;
 		// Create the Character Controller relationship, so we can enable/disable
 		//cc = GameObject.Find ("Player").GetComponent<CharacterController> ();
-		//cc.enabled = true;
+		cc.enabled = true;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		//Allow interaction with the terminal pop up and input box.
-		// Look for the mouse button to be pressed.
-		//if (Input.GetMouseButtonDown (0)) {
-		if (Input.GetKeyDown("e")) {
-			// If the terminal console is marked as active, and a user clicks, then close the terminal
-			// And the global variable is marked as INACTIVE
-			if (GameManager.instance.consoleActive == true) {
-				GameManager.instance.consoleActive = false;
-				if(GameManager.instance.lockCharController == false) {
-					//cc.enabled = true;
-					myCamera.enabled = false;
-				}
-			} else {
-				// If the terminal console is NOT active, and a user clicks, the terminal is opened.
-				// And the global variable is marked as ACTIVE.
-				Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit, 3.0f);
-				// TODO: Getting a weird error here, when clicking elsewhere in the game. Check logic.
-				// Check about using E key to USE the terminal, when close enough to trigger ray
-				if (hit.transform.gameObject.tag == "Terminal") {
-					GameManager.instance.lockCharController = true;
-					if(GameManager.instance.lockCharController == true) {
-						//cc.enabled = false;
-					}
-					GameManager.instance.consoleActive = true;
-					myCamera.enabled = true;
-					canvas.enabled = true;
-					// TODO: Consider moving the destroy NetMgrHUD to main canvas, so it can be closed immediately upon start, by player.
-					NetworkManager.DestroyObject(NetworkManagerHUD);
+//	// Update is called once per frame
+//	void Update () {
+//		//Allow interaction with the terminal pop up and input box.
+//		// Look for the mouse button to be pressed.
+//		//if (Input.GetMouseButtonDown (0)) {
+//		if (Input.GetKeyDown("e")) {
+//			// If the terminal console is marked as active, and a user clicks, then close the terminal
+//			// And the global variable is marked as INACTIVE
+//			if (GameManager.instance.consoleActive == true) {
+//				GameManager.instance.consoleActive = false;
+//				if(GameManager.instance.lockCharController == false) {
+//					//cc.enabled = true;
+//					myCamera.enabled = false;
+//				}
+//			} else {
+//				// If the terminal console is NOT active, and a user clicks, the terminal is opened.
+//				// And the global variable is marked as ACTIVE.
+//				Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit, 3.0f);
+//				// TODO: Getting a weird error here, when clicking elsewhere in the game. Check logic.
+//				// Check about using E key to USE the terminal, when close enough to trigger ray
+//				if (hit.transform.gameObject.tag == "Terminal") {
+//					GameManager.instance.lockCharController = true;
+//					if(GameManager.instance.lockCharController == true) {
+//						//cc.enabled = false;
+//					}
+//					GameManager.instance.consoleActive = true;
+//					myCamera.enabled = true;
+//					canvas.enabled = true;
+//					// TODO: Consider moving the destroy NetMgrHUD to main canvas, so it can be closed immediately upon start, by player.
+//					NetworkManager.DestroyObject(NetworkManagerHUD);
+//
+//				}
+//			}
+//		}
+//	}
 
-				}
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.BackQuote)) {
+			if (inTerminal == false) {
+				myCamera.enabled = true;
+				canvas.enabled = true;
+				inTerminal = true;
+				cc.enabled = false;
+			} else {
+				myCamera.enabled = false;
+				canvas.enabled = false;
+				inTerminal = false;
+				cc.enabled = true;
 			}
 		}
+
 	}
 
 }
